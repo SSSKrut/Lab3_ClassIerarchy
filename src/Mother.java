8b04fdbee6f6549ff875cf8aaf4ec4fc1339cdfe
SSSKrut <m_troc@mail.ru>
2022-12-14 13:01:51 +0300

    1

--- src/Mother.java
+++ src/Mother.java
@@ -2,4 +2,13 @@ public class Mother extends Actor implements Contest{
     Mother(String name){
         super(name);
     }
+    public String bendOver(Summon what){
+        return "Нагнулась над "+what.name+"."
+    }
+    public String mix(Summon what){
+        return "Помешала в "+what.name();
+    }
+    public String say(String what, Summon toWho=){
+
+    }
 }
